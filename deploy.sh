8e02375b86f3cb7d2633cbde9df59292e1dff971
Hoo <sherryho148@gmail.com>
2021-11-16 17:08:27 +0800

    reivse to https

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,7 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
+git branch -M main
 git add -A
 git commit -m 'deploy'
 
@@ -22,9 +23,10 @@ git commit -m 'deploy'
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # 以這個專案來講就要改成這樣以下這樣，下面是走 ssh 模式
-git push -f git@github.com:sherry-hooo/F2E-Week1-Traveling-Site.git main:gh-pages
+# git push -f git@github.com:sherry-hooo/F2E-Week1-Traveling-Site.git main:gh-pages
 
 # 除此之外，也可以改走 HTTPS 模式
 # git push -f https://github.com/sherry-hooo/HexfootMusic.git master:gh-pages
+git push -f https://github.com/sherry-hooo/F2E-Week1-Traveling-Site.git main:gh-pages
 
 cd -
