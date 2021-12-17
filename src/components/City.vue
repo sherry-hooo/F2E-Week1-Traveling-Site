71d4e0a55149edae00713d8f691a118c16a84d59
Hoo <sherryho148@gmail.com>
2021-12-17 17:18:52 +0800

    add city description and bouncing effect

--- src/components/City.vue
+++ src/components/City.vue
@@ -9,37 +9,61 @@
     <div>
       {{ city.cityName }}
     </div>
+    <!-- <p>{{ cityIntroduction }}</p> -->
   </router-link>
 </template>
 
 <script>
+import citiesList from "@/assets/data/citiesList.json";
+
 export default {
   name: "city",
   props: ["city"],
   data() {
     return {};
   },
+  computed: {
+    citiesList() {
+      return citiesList;
+    },
+    cityIntroduction() {
+      console.log(this.citiesList);
+      return this.citiesList.find(
+        (city) => city.cityName === this.city.cityName
+      ).intro;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .city {
-  flex: 0 1 auto;
   padding: 20px 10px;
-  border-radius: 50px;
-  background: #91b4c6;
+  border-radius: 50%;
+  border: 2px solid #91b4c6;
+  background: white;
   font-weight: 500;
   color: rgb(80, 77, 77);
+  transition: all 0.3s ease-in-out;
 
   &:hover {
     background: orange;
+    transition: all 0.3s ease-in-out;
+    animation: bouncing 0.3s infinite alternate;
   }
 
-  @media (min-width: 768px) {
-    padding: 35px 20px;
+  @include breakpoint.tablet {
     font-size: 20px;
+    padding: 30px 15px;
   }
-  @media (min-width: 992px) {
+}
+
+@keyframes bouncing {
+  0% {
+    transform: translateY(0px);
+  }
+  100% {
+    transform: translateY(-10px);
   }
 }
 </style>
